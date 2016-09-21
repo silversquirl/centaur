6fb40e3679e366c0b50474c58d026d1a9d4a9ad5
Samadi van Koten <me@vktec.co.uk>
2016-09-21 22:26:07 +0100

    Improve and comment code, add example file

--- centaur.scm
+++ centaur.scm
@@ -11,19 +11,26 @@
 
   (reexport spiffy)
 
+  ;;; Helpers
+  ;; Remove the prefix from a list
   (define (remove-prefix l pre
                          #!optional
                          (eq equal?))
     (if (and (pair? l)
-             (pair? pre)
-             (eq (car l)
-                 (car pre)))
-        (remove-prefix (cdr l)
-                       (cdr pre)
-                       eq)
+             (pair? pre))
+        (if (eq (car l)
+                (car pre))
+            (remove-prefix (cdr l)
+                           (cdr pre)
+                           eq)
+
+            ;; pre is not a prefix of l
+            #f)
         l))
 
+  ;;; Routing
   (define ((router matchers) continue)
+    ;; Get the URI and strip out unnecessary bits
     (let ((uri (filter
                 (lambda (part)
                   (not (or (eqv? part '/)
@@ -31,24 +38,29 @@
                 (uri-path (request-uri (current-request))))))
       (call/cc
        (lambda (matched)
+         ;; Loop through and run each matcher
          (for-each
           (lambda (matcher)
-            (let* ((reversed-matcher (reverse matcher))
-                   ;; Store the return value
-                   (rv ((car reversed-matcher)
-                        (remove-prefix uri (cdr reversed-matcher)))))
-              (when (response? rv)
-                (matched rv))))
+            (let ((reversed-matcher (reverse matcher))
+                  ;; The unprefixed URI
+                  (unprefixed-uri
+                   (remove-prefix uri (cdr reversed-matcher))))
+              (when (unprefixed-uri)
+                ;; Store the return value
+                (let ((rv ((car reversed-matcher) unprefixed-uri)))
+                  ;; If we've responded, break out of the loop
+                  (when (response? rv)
+                    (matched rv))))))
           matchers)
 
          ;; We've not exited yet, so it's not been matched
          (continue)))))
 
-  (define (serve matchers
-                 #!optional
-                 (port 8080)
-                 (host ".*"))
+  ;;; Convenience
+  (define port server-port)
+  (define host (make-parameter ".*"))
 
+  (define (serve matchers)
     (vhost-map (list (cons host (router matchers))))
-    (server-port port)
     (start-server)))
+
---
+++ example.scm
@@ -0,0 +1,18 @@
+(use centaur matchable)
+
+(print "Starting...")
+(serve
+ `((/
+    ,(match-lambda
+       (()
+        (send-response status: 'ok
+                       body: "<h1>Hello, world!</h1>"))
+
+       ((name)
+        (send-response status: 'ok
+                       body: (string-append
+                              "<h1>Hello, "
+                              name
+                              "!</h1>")))
+       (else #f)))))
+
